7bfbceda0b56a0c119f24a7c24a2903fcb2c1143
Hugo da Silva Gomes <hdsg0807@gmail.com>
2022-06-27 02:22:49 -0300

    charReducer file created

--- src/store/charReducer.ts
+++ src/store/charReducer.ts
@@ -1,21 +1,36 @@
 import { Char } from "../model/Char"
 
-export const charReducer = (state: Char[], action: any) => {
-  switch (action.type) {
-    case "openChar":
-      state[action.payload] = { ...state[action.payload], status: 'opened' }
+interface reducerActionsType {
+  openChar: (state: Char[], action: any) => Char[];
+  closeChars: (state: Char[]) => Char[];
+  completeChars: (state: Char[]) => Char[];
+}
+
+const reducerActions: reducerActionsType = {
+  openChar: (state: Char[], action: any) => {
+    state[action.payload] = { ...state[action.payload], status: 'opened' }
       return [...state];
-    case "closeChars":
-      return state.map((char: Char) => {
-        char.status === "opened" && (char.status = "closed");
-        return char;
-      });
-    case "completeChars":
-      return state.map((char: Char) => {
-        char.status === "opened" && (char.status = "completed");
-        return char;
-      });
-    default:
-      return state;
+  },
+  closeChars: (state: Char[]) => {
+    return state.map((char: Char) => {
+      char.status === "opened" && (char.status = "closed");
+      return char;
+    });
+  },
+  completeChars: (state: Char[]) => {
+    return state.map((char: Char) => {
+      char.status === "opened" && (char.status = "completed");
+      return char;
+    });
   }
+}
+
+export const charReducer = (state: Char[], action: any) => {
+  let fn = reducerActions[action.type as keyof reducerActionsType];
+
+  if (fn) {
+    return fn(state, action);
+  }
+  console.log('[WARNING] Action without reducer:', action);
+  return state;
 };
